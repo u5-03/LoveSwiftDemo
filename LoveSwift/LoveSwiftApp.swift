74e5f07c413177a5a76712faf07b2967502112b1
yugo.sugiyama <sugiy.u.2203@gmail.com>
2024-10-22 19:20:21 +0900

    Initial Commit

---
+++ LoveSwift/LoveSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveSwiftApp.swift
+//  LoveSwift
+//
+//  Created by Yugo Sugiyama on 2024/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LoveSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
